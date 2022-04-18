9cedba3aee911716a0be948baab3ff024cba9c02
viviromero99 <viviromerobr@gmail.com>
2022-04-17 23:23:40 -0300

    inicializando projeto com templates de servidor e cliente

---
+++ client2.py
@@ -0,0 +1,9 @@
+import rpyc
+import sys
+import os
+
+if len(sys.argv) < 2:
+    exit("Usage {} SERVER".format(sys.argv[0]))
+server = sys.argv[1]
+conn = rpyc.connect(server,18861)
+print(c.get_question)
